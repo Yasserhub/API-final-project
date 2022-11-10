e0131852f56401bd3885b4129e7a55fb610692eb
Yasserhub <yasser.a.murshed@gmail.com>
2022-11-10 21:29:48 +0100

    change the APIs

--- src/constants.js
+++ src/constants.js
@@ -5,21 +5,21 @@ export const NEWS_ITEM_ID = 'news-item';
 export const BBC_NEWS_ID =
   'https://newsapi.org/v2/top-headlines?' +
   'sources=bbc-news&' +
-  'apiKey=99cbe1cff8364c3a81096ed76ac849b8';
+  'apiKey=1b38f045434b47bf8882cd8fcba95788';
 
 export const WALL_STREET_ID =
   'https://newsapi.org/v2/everything?' +
   'domains=wsj.com&' +
-  'apiKey=99cbe1cff8364c3a81096ed76ac849b8';
+  'apiKey=1b38f045434b47bf8882cd8fcba95788';
 
 export const TECH_CRUNCH_ID =
   'https://newsapi.org/v2/top-headlines?' +
   'sources=techcrunch&' +
-  'apiKey=99cbe1cff8364c3a81096ed76ac849b8';
+  'apiKey=1b38f045434b47bf8882cd8fcba95788';
 
 export const APPLE_NEWS_ID =
   'https://newsapi.org/v2/everything?' +
   'q=Apple&' +
   'from=2022-11-06&' +
   'sortBy=popularity&' +
-  'apiKey=99cbe1cff8364c3a81096ed76ac849b8';
+  'apiKey=1b38f045434b47bf8882cd8fcba95788';
